f76294a581c15d1036d0f5e3a2d40f81b80e75e5
projecteinf <projecteinf@boscdelacoma.cat>
2024-05-09 11:35:43 -0600

    Reestructuració amb funcions

--- TestTTD/src/app/tests/Components/demo.component.spec.ts
+++ TestTTD/src/app/tests/Components/demo.component.spec.ts
@@ -1,19 +1,28 @@
 import { LightswitchComponent } from "../../Demos/Components/demo.component";
 
 describe('LightswitchComp', () => {
-    it('#clicked() should toggle #isOn', () => {
-      const comp = new LightswitchComponent();  // isOn = false
-      expect(comp.isOn).withContext('off when is created').toBe(false); // isOn és la propietat del component
-      comp.clicked();   // Fem clic
-      expect(comp.isOn).withContext('Opening the light').toBe(true);
-      comp.clicked();
-      expect(comp.isOn).withContext('Closing the light').toBe(true);
-    });
+    testOnOFF();
+    testMessageOnOFF();
   
+});
+
+
+function testMessageOnOFF() {
     it('#clicked() should set #message to "is on"', () => {
-      const comp = new LightswitchComponent();  // create a new instance of the component(isOn = false)
-      expect(comp.message).withContext('off at first').toMatch(/is off/i);
-      comp.clicked();
-      expect(comp.message).withContext('on after clicked').toMatch(/is on/i);
-    }); 
-  });
+        const comp = new LightswitchComponent();  // create a new instance of the component(isOn = false)
+        expect(comp.message).withContext('off at first').toMatch(/is off/i);
+        comp.clicked();
+        expect(comp.message).withContext('on after clicked').toMatch(/is on/i);
+      }); 
+}
+
+function testOnOFF() {
+    it('#clicked() should toggle #isOn', () => {
+        const comp = new LightswitchComponent();  // isOn = false
+        expect(comp.isOn).withContext('off when is created').toBe(false); // isOn és la propietat del component
+        comp.clicked();   // Fem clic
+        expect(comp.isOn).withContext('Opening the light').toBe(true);
+        comp.clicked();
+        expect(comp.isOn).withContext('Closing the light').toBe(false);
+      });
+}
