1ffabeab574d9923eeb261cacfef5a3e7601de28
projecteinf <projecteinf@boscdelacoma.cat>
2024-05-09 11:31:58 -0600

    Explicació de les proves fetes

--- TestTTD/src/app/tests/Components/demo.component.spec.ts
+++ TestTTD/src/app/tests/Components/demo.component.spec.ts
@@ -2,20 +2,18 @@ import { LightswitchComponent } from "../../Demos/Components/demo.component";
 
 describe('LightswitchComp', () => {
     it('#clicked() should toggle #isOn', () => {
-      const comp = new LightswitchComponent();
-      expect(comp.isOn).withContext('off at first').toBe(false);
+      const comp = new LightswitchComponent();  // isOn = false
+      expect(comp.isOn).withContext('off when is created').toBe(false); // isOn és la propietat del component
+      comp.clicked();   // Fem clic
+      expect(comp.isOn).withContext('Opening the light').toBe(true);
       comp.clicked();
-      expect(comp.isOn).withContext('on after click').toBe(true);
-      comp.clicked();
-      expect(comp.isOn).withContext('off after second click').toBe(false);
+      expect(comp.isOn).withContext('Closing the light').toBe(true);
     });
   
     it('#clicked() should set #message to "is on"', () => {
-      const comp = new LightswitchComponent();
-      expect(comp.message)
-        .withContext('off at first')
-        .toMatch(/is off/i);
+      const comp = new LightswitchComponent();  // create a new instance of the component(isOn = false)
+      expect(comp.message).withContext('off at first').toMatch(/is off/i);
       comp.clicked();
       expect(comp.message).withContext('on after clicked').toMatch(/is on/i);
-    });
+    }); 
   });
